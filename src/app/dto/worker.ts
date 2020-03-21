c0f9c6a256a8de082d33b32b46c0912e30713568
Saban Uenlue - netTrek <us@netTrek.de>
2020-03-21 22:28:24 +0100

    feat: dto’s generated
    commit 1807558b3e13d174d50b97175499a590f9434616
    Author: Saban Uenlue - netTrek <us@netTrek.de>
    Date:   Sat Mar 21 22:13:50 2020 +0100
    
        extract dto's
    
    commit 8c9208009baee184708c8fb2bc59d2ff4a8dc5ae
    Author: Saban Uenlue - netTrek <us@netTrek.de>
    Date:   Sat Mar 21 20:36:45 2020 +0100
    
        dto setup

---
+++ src/app/dto/worker.ts
@@ -0,0 +1,12 @@
+import { Tag } from './tag';
+
+export interface Worker {
+  id: number;
+  workModeId: number;
+  disciplineId: number;
+  name: string;
+  stationId: number;
+  teamId: number;
+  tags: Tag[]; // Backend müsste die TAGs n Tage in die Vergangenheit und Zukunft
+  hoursWorkedInCurrentMonth: number;
+}
