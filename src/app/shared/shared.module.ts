bb0cc1b86450c796e91027feea8d14292e5b58b6
Stefan Liebehenz <icevarta@gmail.com>
2020-03-22 08:13:59 +0100

    worker tooltip wip

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
+import {MatCommonModule} from '@angular/material/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
@@ -10,12 +11,14 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    MatCommonModule,
     MatInputModule,
     MatFormFieldModule,
     MatAutocompleteModule,
@@ -24,10 +27,12 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
     MatChipsModule,
     MatTooltipModule,
     MatProgressBarModule,
+    OverlayModule,
     DragDropModule
   ],
   exports: [
     ReactiveFormsModule,
+    MatCommonModule,
     MatInputModule,
     MatFormFieldModule,
     MatAutocompleteModule,
@@ -36,6 +41,7 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
     MatChipsModule,
     MatTooltipModule,
     MatProgressBarModule,
+    OverlayModule,
     DragDropModule
   ]
 })
