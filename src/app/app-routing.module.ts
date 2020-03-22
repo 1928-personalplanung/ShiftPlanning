b5d6a5128083d6c99fd594575c3fef90b6d109e7
Stefan Liebehenz <icevarta@gmail.com>
2020-03-22 08:17:40 +0100

    inclucde routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import { DelMeLaterAComponent } from './del-me-later/del-me-later-a/del-me-later-a.component';
+
+import {MainViewComponent} from './main-view/main-view.component';
 import { DelMeLaterBComponent } from './del-me-later/del-me-later-b/del-me-later-b.component';
 import { DelMeLaterCComponent } from './del-me-later/del-me-later-c/del-me-later-c.component';
 
 const routes: Routes = [
-  { path: '', component: DelMeLaterAComponent },
+  { path: '', component: MainViewComponent },
   { path: 'modalB',
     component: DelMeLaterBComponent,
     outlet: 'modal' },
