5fbec8f2a9df0a94838aaf8758f79fa99a62505f
DeeJayTC <DeeJay@tcdev.de>
2020-03-22 14:18:34 +0100

    Fix client app folder

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace ShiftPlan
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "ClientApp/dist/shift-planning";
             });
 
             
